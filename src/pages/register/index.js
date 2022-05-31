df05ed4b8fdd4be10b69a9c0446e6579fae65a41
PaganMuffin <50595976+PaganMuffin@users.noreply.github.com>
2022-05-31 23:21:46 +0200

    add register without validation

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,5 +1,90 @@
+import { Button, Paper, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import CustomInputWithLabel from "../../components/CustomInputwithLabel";
+import validateEmail from "../../utills";
+
 const Register = () => {
-	return <div>Register</div>;
+	const [email, setEmail] = useState("");
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [passwordConfirm, setPasswordConfirm] = useState("");
+
+	useEffect(() => {
+		console.log("Login page rendered");
+	}, []);
+
+	const checkData = () => {
+		console.log(email);
+	};
+
+	const checkPasswords = () => {
+		if (password === passwordConfirm && password.length > 0) {
+			return true;
+		}
+		return false;
+	};
+
+	const handleSubmit = (e) => {
+		checkData();
+		console.log(email, username, password);
+	};
+	return (
+		<div
+			style={{
+				height: "100vh",
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "center",
+				alignItems: "center",
+			}}>
+			<Paper
+				elevation={10}
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					gap: 10,
+					margin: "auto",
+					width: "400px",
+					alignItems: "center",
+					padding: 50,
+				}}>
+				<Typography variant="h4" style={{ paddingTop: 50, paddingBottom: 50 }}>
+					Zarejestruj się
+				</Typography>
+				<CustomInputWithLabel
+					value={email}
+					setFunction={setEmail}
+					label={"Adres e-mail"}
+					type={"e-mail"}
+				/>
+				<CustomInputWithLabel
+					value={username}
+					setFunction={setUsername}
+					label={"Nazwa użytkownika"}
+					type={"text"}
+				/>
+				<CustomInputWithLabel
+					value={password}
+					setFunction={setPassword}
+					label={"Hasło"}
+					type={"password"}
+				/>
+				<CustomInputWithLabel
+					value={passwordConfirm}
+					setFunction={setPasswordConfirm}
+					label={"Powtórz hasło"}
+					type={"password"}
+				/>
+				<Button
+					variant="contained"
+					sx={{ marginTop: 5 }}
+					onClick={handleSubmit}
+					disabled={!checkPasswords()}>
+					Zarejestruj się
+				</Button>
+			</Paper>
+		</div>
+	);
 };
 
 export default Register;
