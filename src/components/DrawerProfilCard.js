3c31ff71ea59a18dbf2a3fe69140aebf358e2c7c
PaganMuffin <50595976+PaganMuffin@users.noreply.github.com>
2022-06-15 14:08:47 +0200

    add basic user card

--- src/components/DrawerProfilCard.js
+++ src/components/DrawerProfilCard.js
@@ -1,51 +1,49 @@
-import { Avatar, Box, Paper, Typography } from "@mui/material"
-import { Link } from "react-router-dom"
+import { Avatar, Box, Paper, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const DrawerProfilCard = () => {
-    return <Paper  style={{display:'flex', padding:10, gap:10, alignItems:"center"}} >
-        <Link to={"/user/1/PaganMuffin"} style={{
-                    textDecoration:"none",
-                    color:"black"
-                }}>
-                <Avatar  sx={{ width: 56, height: 56 }}/>
-        </Link>        
-        <Box>
-            <Link to={"/user/1/PaganMuffin"} style={{
-                    textDecoration:"none",
-                    color:"black"
-                }}>
-                
-                <Typography variant="h6">
-                    PaganMuffin
-                </Typography>
-            </Link>
-            
-            <Box>
-                <Link to={"/user/settings"} style={{
-                    textDecoration:"none",
-                    color:"black"
-                }}>
-                    <Typography variant="subtitle2">
-                        Ustawienia
-                    </Typography>
-                </Link>
-                <Link to={"/user/messeges"} style={{
-                    textDecoration:"none",
-                    color:"black"
-                }}>
-                    <Typography variant="subtitle2">
-                        Wiadomosci
-                    </Typography>
-                </Link>
-            </Box>
-            
-            
-            
-            
-            
-            
-        </Box>
-    </Paper>
-}
+	return (
+		<Paper
+			style={{ display: "flex", padding: 10, gap: 10, alignItems: "center" }}>
+			<Link
+				to={"/user/1/PaganMuffin"}
+				style={{
+					textDecoration: "none",
+					color: "black",
+				}}>
+				<Avatar sx={{ width: 56, height: 56 }} />
+			</Link>
+			<Box>
+				<Link
+					to={"/user/1/PaganMuffin"}
+					style={{
+						textDecoration: "none",
+						color: "black",
+					}}>
+					<Typography variant="h6">PaganMuffin</Typography>
+				</Link>
 
-export default DrawerProfilCard
+				<Box>
+					<Link
+						to={"/user/settings"}
+						style={{
+							textDecoration: "none",
+							color: "black",
+						}}>
+						<Typography variant="subtitle2">Ustawienia</Typography>
+					</Link>
+					<Link
+						to={"/user/messeges"}
+						style={{
+							textDecoration: "none",
+							color: "black",
+						}}>
+						<Typography variant="subtitle2">Wiadomosci</Typography>
+					</Link>
+				</Box>
+			</Box>
+		</Paper>
+	);
+};
+
+export default DrawerProfilCard;
--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,8 +1,33 @@
+import { Avatar, Paper, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+const demoUser = {
+	id: 1,
+	username: "PaganMuffin",
+	avatar: "",
+	subCount: 120,
+};
+
+const demoLatestSubs = [{}];
+
 const User = () => {
+	const [data, setData] = useState(null);
+	const [subs, setSubs] = useState([]);
 	const params = useParams();
-	return <div>{JSON.stringify(params)}</div>;
+	useEffect(() => {
+		setData(demoUser);
+	}, []);
+	if (data == null) return <div>LOADING</div>;
+	return (
+		<div>
+			<Paper>
+				<Avatar sx={{ width: 56, height: 56 }} />
+				<Typography>{data.username}</Typography>
+				<Typography>{data.subCount}</Typography>
+			</Paper>
+		</div>
+	);
 };
 
 export default User;
