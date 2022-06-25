8231462034f11e491021dc3df78abf749dc0ad45
PaganMuffin <50595976+PaganMuffin@users.noreply.github.com>
2022-06-26 01:43:16 +0200

    link search with api

--- src/components/addComponents/StepOne.js
+++ src/components/addComponents/StepOne.js
@@ -1,12 +1,40 @@
-import { Paper, Box, Typography } from "@mui/material";
+import { Paper, Box, Typography, Button } from "@mui/material";
 import AniListSeriesCard from "./AniListSeriesCard";
 import SearchBar from "../SearchBar";
 import { useEffect, useState } from "react";
-
-import demoData from "./demoData.json";
+import { useDebounce } from "../../utills";
 
 const StepOne = ({ value, setFunction }) => {
 	const [search, setSearch] = useState("");
+	const [searchResults, setSearchResults] = useState([]);
+	const debounceSearchTerm = useDebounce(search, 500);
+	const [page, setPage] = useState(1);
+	const [hasNextPage, setHasNextPage] = useState(true);
+
+	useEffect(() => {
+		(async () => {
+			const api_url = new URL(process.env.REACT_APP_API_URL);
+			api_url.pathname = "/searchAniList";
+			const queryString = new URLSearchParams();
+			queryString.append("p", page);
+			if (debounceSearchTerm != "") queryString.append("q", debounceSearchTerm);
+			api_url.search = queryString.toString();
+			const f = await fetch(api_url.toString());
+			const f_data = await f.json();
+			setSearchResults(f_data.data.Page.media);
+			setHasNextPage(f_data.data.Page.pageInfo.hasNextPage);
+		})();
+	}, [debounceSearchTerm, page]);
+
+	const incrementPage = () => {
+		setFunction(-1);
+		if (hasNextPage) setPage(page + 1);
+	};
+
+	const decrementPage = () => {
+		setFunction(-1);
+		if (page > 1) setPage(page - 1);
+	};
 
 	return (
 		<Paper
@@ -26,7 +54,7 @@ const StepOne = ({ value, setFunction }) => {
 				setFunction={setSearch}
 			/>
 			<Box style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
-				{demoData.slice(0, 1).map((data, idx) => {
+				{searchResults.map((data, idx) => {
 					return (
 						<AniListSeriesCard
 							key={idx}
@@ -37,6 +65,35 @@ const StepOne = ({ value, setFunction }) => {
 						/>
 					);
 				})}
+			</Box>
+			<Box
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "space-between",
+					width: "100%",
+				}}>
+				<Button
+					disabled={page === 1}
+					onClick={decrementPage}
+					style={{
+						color:
+							page === 1 ? `rgba(${process.env.REACT_APP_TEXT},0.2)` : null,
+					}}
+					variant="contained">
+					Poprzednia strona
+				</Button>
+				<Button
+					disabled={!hasNextPage}
+					style={{
+						color: !hasNextPage
+							? `rgba(${process.env.REACT_APP_TEXT},0.2)`
+							: null,
+					}}
+					onClick={incrementPage}
+					variant="contained">
+					Następna strona
+				</Button>
 			</Box>
 		</Paper>
 	);
